1df37f1b46636d7b94810046275ef7bc36cddfc1
EstherGaldeano <egaldeano@uxland.es>
2021-02-19 21:56:30 +0100

    Menu > Keyboard selection available

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,16 +1,22 @@
 extends Control
 var scene_path_to_load
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	#keyboard selection available
+	$Menu/CenterRow/Buttons/NewGameButton.grab_focus()
+	
+	#change scene when click a button, patch scene added at the editor inspector
 	for button in $Menu/CenterRow/Buttons.get_children():
 		button.connect("pressed",self,"on_Button_pressed", [button.scene_to_load])
 
+
+	#fade in when a scene is selected  
 func on_Button_pressed(scene_to_load):
 	scene_path_to_load = scene_to_load
 	$FadeIn.show()
 	$FadeIn.fade_in()
-
+	
+	
+	#when the fade in finished, show the selected scene
 func _on_FadeIn_fade_finished():
 	get_tree().change_scene(scene_path_to_load)
