2eb3163eac1855ec24da52bda33f8eec7fff75d8
RicardRV <43618855+RicardRV@users.noreply.github.com>
2021-02-20 22:50:28 +0100

    Riku: Added dynamic goals

--- Game/Goal.gd
+++ Game/Goal.gd
@@ -1,8 +1,7 @@
 extends Area2D
 
-signal goalAreaEntered
+signal goalColision
 
-
-func _on_Goal_area_entered(area):
-	emit_signal("goalAreaEntered")
-
+func _on_Goal_body_entered(body):
+	emit_signal("goalColision")
+	queue_free()
--- Scripts/MapScript.gd
+++ Scripts/MapScript.gd
@@ -10,16 +10,13 @@ func _ready():
 	rng.randomize()
 	generatePlayer(tileMapUsedCells[1])
 	generateGoal()
-	var goalNode = self.get_node("Goal")
-	print(goalNode.get_tree())
-	goalNode.connect("goalAreaEntered",self,"resetGoal")
-
+	
 func resetGoal():
-	print("patata XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD")
+	generateGoal()
 
 func generateGoal():
 	var handUpOrDown = rng.randi_range(1,2)
-	var goalPos = rng.randi_range(10,27)
+	var goalPos = rng.randi_range(10,tileMapUsedCells[tileMapUsedCells.size()-1].x)
 	var goal_instance = Goal.instance()
 	add_child(goal_instance)
 	if handUpOrDown == 1:
@@ -27,9 +24,9 @@ func generateGoal():
 	else:
 		goal_instance.position = $TileMap.map_to_world(Vector2(goalPos, (tileMapUsedCells[tileMapUsedCells.size() - 1].y + 1) ))
 		goal_instance.rotation_degrees += 180
+	goal_instance.connect("goalColision",self,"resetGoal")
 
 func generatePlayer(position):
 	var player_instance = Player.instance()
 	add_child(player_instance)
 	player_instance.position = $TileMap.map_to_world(position + Vector2(0,2)) 
-	
