12482c0ca3a5eeb9e22cc760b890d33ecf9e2779
RicardRV <43618855+RicardRV@users.noreply.github.com>
2021-02-21 23:31:26 +0100

    Riku: Hardcodeando el tutorial

--- Scripts/Goal.gd
+++ Scripts/Goal.gd
@@ -1,16 +1,21 @@
 extends Area2D
 
 signal goalColision(body)
-signal addObstacle
+signal addObstacle(isFirst)
 
 var goalPoints = 0
 
 func _on_Goal_body_entered(body):
-	addGoalPoints()
-	emit_signal("goalColision",self)
-	
-func addGoalPoints():
 	goalPoints += 1
+	if goalPoints == 1:
+		emit_signal("addObstacle",true)
+	else:
+		checkToAddObstacles()
+		emit_signal("goalColision",self)
+
+	
+func checkToAddObstacles():
+	
 	print(goalPoints)
 	if goalPoints % 3 == 0:
-		emit_signal("addObstacle")
+		emit_signal("addObstacle",false)
--- Scripts/MapScript.gd
+++ Scripts/MapScript.gd
@@ -1,39 +1,22 @@
 extends Node2D
 
 
-const Player = preload("res://Game/Beer.tscn")
-const Goal = preload("res://Game/Goal.tscn")
+
 const Obstacle = preload("res://Game/Obsctacle.tscn")
-const GameManager = preload("res://Game/GameManager.tscn")
 onready var rng = RandomNumberGenerator.new()
 onready var tileMapUsedCells = $TileMap.get_used_cells()
 onready var mapSort = $MapSort
+onready var Player = $MapSort/Beer
+onready var Goal = $MapSort/Goal
 
 var goalPoints = 0
 
 func _ready():
 	rng.randomize()
-	generatePlayer(tileMapUsedCells[1])
-	generateGoal()
+	Goal.connect("goalColision",self,"resetGoalPosition")
+	Goal.connect("addObstacle",self,"generate_Obstacle")
 
 
-func generateGoal():
-	var handUpOrDown = rng.randi_range(1,2)
-	var goalPos = rng.randi_range(10,tileMapUsedCells[tileMapUsedCells.size()-1].x)
-	var goal_instance = Goal.instance()
-	mapSort.add_child(goal_instance)
-	if handUpOrDown == 1:
-		goal_instance.position = $TileMap.map_to_world(Vector2(goalPos,tileMapUsedCells[0].y))
-	else:
-		goal_instance.position = $TileMap.map_to_world(Vector2(goalPos, (tileMapUsedCells[tileMapUsedCells.size() - 1].y + 1) ))
-		goal_instance.rotation_degrees += 180
-	goal_instance.connect("goalColision",self,"resetGoalPosition")
-	goal_instance.connect("addObstacle",self,"generate_Obstacle")
-
-func generatePlayer(position):
-	var player_instance = Player.instance()
-	mapSort.add_child(player_instance)
-	player_instance.position = $TileMap.map_to_world(position + Vector2(0,2)) 
 
 func resetGoalPosition(body):
 	var handUpOrDown = rng.randi_range(1,2)
@@ -46,11 +29,33 @@ func resetGoalPosition(body):
 		body.rotation_degrees += 180
 	goalPoints += 1
 
-func generate_Obstacle():
-	var obstaclePosX = rng.randi_range(3,tileMapUsedCells[tileMapUsedCells.size()-1].x)
-	var obstaclePosY = rng.randi_range(tileMapUsedCells[0].y,tileMapUsedCells[tileMapUsedCells.size()-1].y)
+func generate_Obstacle(isFirst):
 	var obstacle_instance = Obstacle.instance()
+	var obstaclePosX = 25
+	var obstaclePosY = 4
+	if not isFirst:
+		obstaclePosX = rng.randi_range(3,tileMapUsedCells[tileMapUsedCells.size()-1].x)
+		obstaclePosY = rng.randi_range(tileMapUsedCells[0].y,tileMapUsedCells[tileMapUsedCells.size()-1].y)
 	mapSort.add_child(obstacle_instance)
-	obstacle_instance.position = $TileMap.map_to_world(Vector2(obstaclePosX,obstaclePosY))
+	obstacle_instance.position = $TileMap.map_to_world(Vector2(25,4))
+	
+
+#func generateGoal():
+#	var handUpOrDown = rng.randi_range(1,2)
+#	var goalPos = rng.randi_range(10,tileMapUsedCells[tileMapUsedCells.size()-1].x)
+#	var goal_instance = Goal.instance()
+#	mapSort.add_child(goal_instance)
+#	if handUpOrDown == 1:
+#		goal_instance.position = $TileMap.map_to_world(Vector2(goalPos,tileMapUsedCells[0].y))
+#	else:
+#		goal_instance.position = $TileMap.map_to_world(Vector2(goalPos, (tileMapUsedCells[tileMapUsedCells.size() - 1].y + 1) ))
+#		goal_instance.rotation_degrees += 180
+#	goal_instance.connect("goalColision",self,"resetGoalPosition")
+#	goal_instance.connect("addObstacle",self,"generate_Obstacle")
+
+#func generatePlayer(position):
+#	var player_instance = Player.instance()
+#	mapSort.add_child(player_instance)
+#	player_instance.position = $TileMap.map_to_world(position + Vector2(0,2)) 
 
 
